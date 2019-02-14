bbc18763365127e02d3c36a78b2db54fbfdad2c8
patrol7171 <patrol7171@gmail.com>
2019-02-14 12:20:07 -0500

    updated app file

--- hr_dashboard/app.py
+++ hr_dashboard/app.py
@@ -35,6 +35,7 @@ from dateutil.parser import parse
 from collections import defaultdict, ChainMap, OrderedDict
 pd.options.mode.chained_assignment = None
 import mysql.connector
+
 from matplotlib import cm
 current_palette = sns.color_palette("muted", n_colors=30)
 cmap1 = cm.get_cmap('gist_rainbow')
@@ -61,8 +62,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # from config import mysql_pswd
 # PASSWORD = mysql_pswd
 # MYSQLCONNECTION = 'mysql://root:'+PASSWORD+'@localhost/dental_magic_hr_db'
+MYSQLCONNECTION = 'mysql+mysqldb://b1f2489c9b282e:e9c72da2@us-cdbr-iron-east-03.cleardb.net/heroku_ef3c242f95c079b'
 ####### FOR HEROKU DEPLOYMENT ONLY ########:
-MYSQLCONNECTION = os.environ.get('CLEARDB_DATABASE_URL')
+# MYSQLCONNECTION = os.environ.get('CLEARDB_DATABASE_URL')
 
 
 
